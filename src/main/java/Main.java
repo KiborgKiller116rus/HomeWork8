38214726f5ec8ce570b31cce44f38f53fd3cfc60
Damir <KiborgKiller116rus@yandex.ru>
2023-08-15 20:39:33 +0300

    add src

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,15 @@
 public class Main {
-
     public static void main(String[] args) {
         Post post = new Post();
         post.birthday = new FormDate();
-        post.birthday.day = "03";
-        post.birthday.month = "8";
-        post.birthday.year = "1995";
         post.name = "Дамир";
-        post.passport = "4444 № 44444444";
         post.patronymic = "Ильдарович";
-        post.phone = "+7 (999) 999-99-99";
         post.surname = "Сираев";
-        post.subscription = "true";
+        post.birthday.day = 3;
+        post.birthday.month = 8;
+        post.birthday.year = 1995;
+        post.passport = "9999 № 999999";
+        post.phone = "+7 (999) 999 99-99";
+        post.subscription = true;
     }
 }
